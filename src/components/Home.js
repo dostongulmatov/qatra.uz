e9d70a7b9a079507d0172df9e5f5aac4881c4ef6
dostongulmatov <58285468+dostongulmatov@users.noreply.github.com>
2020-10-16 15:49:25 +0500

    checking git

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,13 +10,6 @@ const Home = () => {
         <Card1 />
         <Card1 />
       </div>
-      <div className="card-container">
-        <Card1 />
-        <Card1 />
-        <Card1 />
-        <Card1 />
-      </div>
-
       <Media />
     </div>
   )
