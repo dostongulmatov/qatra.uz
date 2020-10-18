14b2e6f5dcdc92f76fb2b75bdca9d07f6d8b580c
dostongulmatov <58285468+dostongulmatov@users.noreply.github.com>
2020-10-18 15:50:02 +0500

    change Card design

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,99 @@
 import React from 'react'
+import help from './images/help.png'
+import guide from './images/guide.png'
+import question from './images/question.png'
+import advice from './images/advice.png'
+
 
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>We are small non-profit organisation. We want to help rich people to invest their money for the hereafter.</p>
+    <div className="about-page">
+
+      <div className="row h-100">
+        <div className="col-lg-6  my-auto">
+          <p className="what-we-do">What we do?</p>
+          <h1 className="what-we-do">We are small non-profit organisation. We want to help rich people to invest their money for the hereafter.
+          You can use our website or app for making donation at any time at any position to person who really deserve it.</h1>
+        </div>
+        <div className="col-lg-6">
+          <img src={ help }/>
+        </div>
+      </div>
+
+      <div className="row h-100">
+        <div className="col-lg-6">
+          <img src={ guide }/>
+        </div>
+        <div className="col-lg-6 my-auto">
+          <p className="what-we-do">How can you help us?</p>
+          <h1 className="what-we-do">If you are familiar with any person who need help just contact us and fill the form by clicking this button.
+          <button className="btn btn-outline-danger btn-lg ml-2">Need Help</button></h1>
+        </div>
+      </div>
+
+      <div className="row h-100">
+        <div className="col-lg-6 my-auto">
+          <p className="what-we-do">How it works?</p>
+          <h1 className="what-we-do">It is easy. If you have spare money you should just press donate button.
+          if you need help just press need help button. </h1>
+        </div>
+        <div className="col-lg-6">
+          <img src={ question }/>
+        </div>
+      </div>
+
+      <div className="row h-100">
+        <div className="col-lg-6">
+          <img src={ advice }/>
+        </div>
+        <div className="col-lg-6 my-auto">
+          <p className="what-we-do">Why I need to use Qatra.uz</p>
+          <h1 className="what-we-do">There are lots of reasons of using Qatra. The most important is you can make donation whenever you
+          want you will not have to go anywhere for finding need people just sit at home an invest for your future. </h1>
+        </div>
+      </div>
+      <div className="row h-100">
+        <div className="col-6 my-auto">
+          <h1>What is your opinion about our Project and if you want to become partner with us <button className="btn btn-outline-primary">
+          Press this button</button></h1>
+        </div>
+        <div className="col-6">
+          <textarea className="about-page-textarea" rows="7" placeholder="Enter your thoughts and improvements can be added"></textarea>
+          <button className="about-page-btn btn btn-outline-primary">Send your opinions</button>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default About
+
+      {/* <div className="" >
+        <div className="about-containers row">
+          <div className="col-6"> */}
+                    {/* </div>
+          <div className="col-6"> */}
+                    {/* </div>
+        </div>
+        <div className="about-containers right-about-containers">
+          <div> */}
+                  {/* </div>
+          <div> */}
+                    {/* </div>
+        </div>
+      <div className="row" >
+      <div>
+
+      </div>
+      <div>
+
+      </div> */}
+      {/* <div className="about-containers"> */}
+              {/* </div>
+        <div className="about-containers right-about-containers">
+          <div> */}
+                    {/* </div>
+          <div> */}
+                    {/* </div>
+        </div>
+      </div> */}
--- src/components/NeedHelp.js
+++ src/components/NeedHelp.js
@@ -3,7 +3,30 @@ import React from 'react'
 const NeedHelp = () => {
   return (
     <div>
-      <p>Need help page is working</p>
+      <h5 className="need-help-heading">Do you really need help? If yes. Just fill the blanks below and get help you need from kind people.</h5>
+      <div  className="need-help-form">
+        <form>
+          <label>Type your name:</label><input className="need-help-input"  placeholder="Enter your name"/><br />
+          <label>Type your address:</label><input className="need-help-input"  placeholder="Enter your full address"/><br />
+          <label>Enter your phone number:</label><input className="need-help-input"  placeholder="Type your number"/><br />
+          <label>Enter an Avatar image</label><input type="file" /><br />
+          <label>What kind of person are you?</label>
+          <select  className="need-help-input" id="character" name="character">
+            <option value="student">Student</option>
+            <option value="poor">Poor Person</option>
+            <option value="disable">Disable or Seek</option>
+            <option value="fund">head of fund</option>
+            <option value="fund">Masjid departure</option>
+            <option value="fund">Other</option>
+          </select><br />
+          <label>What do you really need or how much money do you need?<br /> Write all things you need but be honest!</label>
+          <textarea placeholder="Enter your needs"/><br />
+          <label>Why do you need money?(Please write all reasons of your need for help)</label><br />
+          <textarea placeholder="Enter reasons which are making you need for money?" className="need-help-reason" rows="8"/>
+          <br />
+          <button className="need-help-btn btn btn-outline-light">submit</button>
+        </form>
+      </div>
     </div>
   )
 }
